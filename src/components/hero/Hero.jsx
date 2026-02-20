351511d57179c83ed750a1766a54490fb0546335
rimba sianturi <rimbasianturi17@gmail.com>
2026-02-20 11:50:14 +0700

    feat:fix warning eager or priority next image

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -14,19 +14,18 @@ const Hero = () => {
           src={dekstopBg}
           alt="desktop-bg"
           className="hidden lg:block h-auto w-[130%] max-w-none -translate-y-[100px]"
-          priority
+          loading="eager"
         />
         <Image
           alt="mobileBg"
           src={mobileBg}
           className="w-full h-auto object-cover lg:hidden"
-          loading="eager"
         />
         <Image
           alt="hero-image"
           src={heroImage}
           className="w-full h-auto absolute top-0 left-1/2 -translate-x-1/2 px-6 -translate-y-[10%] lg:w-[90%] lg:px-0 lg:top-[-12%] lg:left-auto lg:right-[-14%]  lg:translate-x-0 lg:translate-y-0"
-          loading="eager"
+          priority
         />
       </div>
       <div className="max-w-container mx-auto px-5 text-center flex flex-col gap-6 pb-14 -mt-12 lg:gap-5 lg:text-start lg:px-14 lg:py-44 xl:pt-0 xl:mt-10">
